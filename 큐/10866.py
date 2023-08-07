c04607603cefe985a334e9e3b9a99ea3f01382fa
choyunhee <xornd1110@naver.com>
2023-08-07 14:58:36 +0900

    10845,10866

---
+++ 큐/10866.py
@@ -0,0 +1,31 @@
+import sys 
+from collections import deque
+
+N = int(sys.stdin.readline())
+queue = deque([])
+
+for _ in range(N):
+    cmd = sys.stdin.readline().split()
+    if cmd[0] == 'push_back':
+        queue.append(cmd[1])
+    elif cmd[0] == 'push_front':
+        queue.appendleft(cmd[1])
+    elif cmd[0] == 'size':
+        print(len(queue))
+    elif cmd[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    else:
+        if len(queue) == 0:
+            print(-1)
+            continue
+        if cmd[0] == 'pop_front':
+            print(queue.popleft())
+        elif cmd[0] == 'pop_back':
+            print(queue.pop())
+        elif cmd[0] == 'front':
+            print(queue[0])
+        elif cmd[0] == 'back':
+            print(queue[-1])
